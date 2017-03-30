334dbf390036a512b2f43f279c6b8a42c4f3194f
Kiran Challa <kichalla@microsoft.com>
2017-03-30 10:28:04 -0700

    Added functional tests for JsonPatch

---
+++ test/WebSites/FormatterWebSite/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace FormatterWebSite
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ test/WebSites/FormatterWebSite/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+
+namespace FormatterWebSite
+{
+    public class Order
+    {
+        public string OrderName { get; set; }
+
+        [JsonConverter(typeof(ReplaceOrderTypeConverter))]
+        public string OrderType { get; set; }
+    }
+}
